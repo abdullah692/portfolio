94b045f1b8cf6f7a2c48d1e113538e02492fecf7
abdullah692 <abd.siddiqui987@gmail.com>
2024-12-22 20:48:03 +0500

    reusme updated

--- constants/index.js
+++ constants/index.js
@@ -1,2 +1,2 @@
 export const resumeUrl =
-  "https://drive.google.com/file/d/1KIbGOycYhpGDPgMdjIcbWcOZrhU8T3MI/view?usp=sharing";
+  "https://drive.google.com/file/d/1XPWvF9oXvxzlkCDLn-nLoDINYQoa0qwD/view?usp=drive_link";
